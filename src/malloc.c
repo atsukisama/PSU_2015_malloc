f679096d2be4a672f6b5a53087dc917968775529
kerebe_p <paul.kerebel@epitech.eu>
2016-01-26 11:31:04 +0100

    update

--- src/malloc.c
+++ src/malloc.c
@@ -8,10 +8,37 @@
 ** Last update Mon Jan 25 23:52:08 2016 Thomas Martins
 */
 
+#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <unistd.h>
 #include "struct.h"
 
-void *malloc(void *nb)
+void		*my_malloc(size_t size)
 {
+  void *start;
+  void *requested;
 
-  return (nb);
+  start = sbrk(0);
+  requested = sbrk(size);
+  if (requested == (void*) -1)
+    return NULL;
+  if (start == requested);
+  {
+    printf("add : %p\n", requested);
+    return (start);
+  }
+}
+
+int		main(void)
+{
+  char		*str;
+  char		*other;
+
+  str = my_malloc(sizeof(char) * 5);
+  other = my_malloc(sizeof(char) * 10);
+  strcpy(str, "salaaaaaaa");
+  //printf("%s\n", str);
+  return (0);
 }
