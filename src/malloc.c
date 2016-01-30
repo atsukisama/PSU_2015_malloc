5a3ca96ce879a09943c050665d4548cea9b97217
thomas.martins <thomas.martins@epitech.eu>
2016-01-30 04:28:10 +0100

    lol

--- src/malloc.c
+++ src/malloc.c
@@ -5,7 +5,7 @@
 ** Login   <mart_4@epitech.net>
 **
 ** Started on  Mon Jan 25 23:52:07 2016 Thomas Martins
-** Last update Sat Jan 30 02:23:07 2016 Thomas martin
+** Last update Sat Jan 30 04:26:56 2016 Thomas martin
 */
 
 #include <stdlib.h>
@@ -31,26 +31,26 @@ void		check_split(t_block *current, size_t size)
     }
 }
 
-/* FIN DE PLAGE
-t_block		*check_split(t_block *current, size_t size)
-{
-  t_block	*new_block;
+/* FIN DE PLAGE*/
+/* t_block		*check_split(t_block *current, size_t size) */
+/* { */
+/*   t_block	*new_block; */
 
-  if (current->block_size > (size + sizeof(t_block)))
-    {
-      current->block_size = current->block_size - sizeof(t_block) - size;
-      new_block = (void*)current + current->block_size + sizeof(t_block);
-      new_block->block_size = size;
-      new_block->next = current->next;
-      current->next = new_block;
-      new_block->prev = current;
-      new_block->magic = MAGIC;
-      new_block->is_free = true;
-      return (new_block);
-    }
-  return (current);
-}
-*/
+/*   if (current->block_size > (size + sizeof(t_block))) */
+/*     { */
+/*       current->block_size = current->block_size - sizeof(t_block) - size; */
+/*       new_block = (void*)current + current->block_size + sizeof(t_block); */
+/*       new_block->block_size = size; */
+/*       new_block->next = current->next; */
+/*       current->next = new_block; */
+/*       new_block->prev = current; */
+/*       new_block->magic = MAGIC; */
+/*       new_block->is_free = true; */
+/*       return (new_block); */
+/*     } */
+/*   return (current); */
+/* } */
+
 void		*grow_mem(t_block *block, size_t size)
 {
   if (block->block_size >= size)
@@ -72,31 +72,34 @@ size_t		aligned_size(size_t size)
 
 void		*malloc(size_t size)
 {
-  (void)size;
-  /* t_block	*new_block; */
-  /* void		*pointer; */
+  t_block	*new_block;
+  void		*pointer;
 
-  /* new_block = NULL; */
-  /* pointer = NULL; */
-  /* if ((intptr_t)size <= 0) */
-  /*   return (NULL); */
-  /* size = aligned_size(size); */
-  /* if ((new_block = check_block(size)) == NULL) */
-  /*   new_block = ask_mem(size); */
-  /* if (new_block == NULL) */
-  /*   return (NULL); */
-  /* //new_block = check_split(new_block, size); FIN DE PLAGE */
-  /* check_split(new_block, size); */
-  /* new_block->is_free = false; */
-  /* pointer = new_block + 1; */
-  /* pointer = grow_mem(new_block, size); */
+  new_block = NULL;
+  pointer = NULL;
+  if ((intptr_t)size <= 0)
+    return (NULL);
+  size = aligned_size(size);
+  if ((new_block = check_block(size)) == NULL)
+    new_block = ask_mem(size);
+  if (new_block == NULL)
+    return (NULL);
+  //new_block = check_split(new_block, size); FIN DE PLAGE
+  check_split(new_block, size);
+  new_block->is_free = false;
+  pointer = new_block + 1;
+  pointer = grow_mem(new_block, size);
 
-  return (NULL);
+  return (pointer);
 }
 
-void	*realloc(void *ptr, size_t size)
+void		*realloc(void *ptr, size_t size)
 {
-  (void)size;
-  (void)ptr;
-  return (NULL);
+  t_block	*block;
+  size_t	rsize;
+
+  block = ptr - sizeof(t_block);
+  rsize = block->block_size;
+  free(ptr);
+  return (malloc(rsize + size));
 }
