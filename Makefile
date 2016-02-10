414c7db3391b42dc33850e5f89d72b9f272f3492
kerebe_p <paul.kerebel@epitech.eu>
2016-02-10 06:21:57 +0100

    update

---
+++ Makefile
@@ -0,0 +1,50 @@
+##
+## Makefile<2> for  in /home/mart_4/rendu/PSU_2015_malloc
+##
+## Made by Thomas Martins
+## Login   <mart_4@epitech.net>
+##
+## Started on  Mon Jan 25 23:08:27 2016 Thomas Martins
+## Last update Sat Jan 30 02:32:36 2016 Thomas martin
+##
+
+NAME 	= libmy_malloc_$(HOSTTYPE).so
+
+LINK	= libmy_malloc.so
+
+CC      = gcc
+
+SRC	= malloc.c 		\
+	  block.c 		\
+	  block_manip.c		\
+
+RM      = rm -f
+
+OBJ	= $(SRC:.c=.o)
+
+RM	= rm -f
+
+#CFLAGS	+= -W -Wall -Wextra -Werror -fPIC
+CFLAGS	+= -fPIC -g
+LDFLAGS	+= -shared -fPIC -lpthread
+
+CC	= gcc
+
+all:	$(NAME)
+
+$(NAME): $(OBJ)
+	$(CC) -o $(NAME) $(LDFLAGS) $(OBJ)
+	ln -sf $(NAME) $(LINK)
+
+
+%.o : %.c
+	$(CC) -c $(CFLAGS) -o $@ $^
+
+clean:
+	$(RM) $(OBJ)
+
+fclean: clean
+	$(RM) $(LINK)
+	$(RM) $(NAME)
+
+re: fclean all
---
+++ block.c
@@ -0,0 +1,82 @@
+/*
+** block.c for block in /home/kerebe_p/Documents/Epitech/malloc
+** 
+** Made by kerebe_p
+** Login   <paul.kerebel@epitech.eu>
+** 
+** Started on  Sun Jan 31 03:35:35 2016 kerebe_p
+** Last update Sun Jan 31 03:35:35 2016 kerebe_p
+*/
+
+#include "malloc.h"
+
+size_t		aligned_size(size_t size, size_t align)
+{
+  size = size + (align - (size % align));
+  return (size);
+}
+
+t_block		*check_block(size_t size, t_block *g_mem)
+{
+  t_block	*tmp;
+  t_block	*ret;
+  t_block	*end;
+
+  ret = NULL;
+  end = NULL;
+  tmp = g_mem;
+  while (tmp != NULL)
+    {
+      if ((tmp->p_size >= size) && (tmp->is_free == TRUE))
+	{
+	  if (((ret != NULL) && (ret->p_size > tmp->p_size)) ||
+	      (ret == NULL))
+	    ret = tmp;
+	}
+      tmp = tmp->next;
+    }
+  return (ret);
+}
+
+t_block		*ask_mem(size_t size)
+{
+  t_block	*block;
+
+  block = sbrk(0);
+  if (sbrk(aligned_size(size + sizeof(t_block), sysconf(_SC_PAGESIZE))) == (void*)-1)
+    {
+      if (sbrk(size + sizeof(t_block)) == (void*)-1)
+	return (NULL);
+      block->p_size = size;
+      return (block);
+    }
+  block->p_size = aligned_size(size + sizeof(t_block), sysconf(_SC_PAGESIZE)) - sizeof(t_block);
+  return (block);
+}
+
+t_block		*create_block(size_t size, t_block **g_mem)
+{
+  t_block	*new_block;
+  t_block	*tmp;
+
+  tmp = *g_mem;
+  new_block = ask_mem(size);
+  if (new_block == NULL)
+    return (NULL);
+  while (tmp != NULL && tmp->next != NULL)
+    tmp = tmp->next;
+  if (*g_mem == NULL)
+    {
+      *g_mem = new_block;
+      new_block->prev = NULL;
+    }
+  else
+    {
+      tmp->next = new_block;
+      new_block->prev = tmp;
+    }
+  new_block->next = NULL;
+  new_block->m_key = MAGIC;
+  new_block->is_free = TRUE;
+  return (new_block);
+}
