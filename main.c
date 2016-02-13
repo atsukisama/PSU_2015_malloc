ec560996728effc47ffd60107c66c94cfbda09f1
kerebe_p <paul.kerebel@epitech.eu>
2016-02-13 17:11:50 +0100

    update

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+** main.c for main in /home/kerebe_p/Documents/Epitech/malloc
+** 
+** Made by kerebe_p
+** Login   <paul.kerebel@epitech.eu>
+** 
+** Started on  Sun Jan 31 14:40:19 2016 kerebe_p
+** Last update Sun Feb  7 21:33:33 2016 Thomas Martins
+*/
+
+#include "malloc.h"
+
+int		main(void)
+{
+  printf("%zu\n", sizeof(int));
+  return 0;
+}
